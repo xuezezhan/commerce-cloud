7d286a8a6e9734a4d02ac2e338e67bd6431cbe1c
xuezezhan <xuezz@htah.com.cn>
2020-01-14 11:14:39 +0800

    json

--- core-customize/hybris/bin/custom/zdds/zddsstorefront/web/commonwebsrc/acceleratorstorefrontcommons/de/hybris/platform/acceleratorstorefrontcommons/forms/payment/MerchantCallbackUrlStrategy.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
- */
-package de.hybris.platform.acceleratorstorefrontcommons.forms.payment;
-
-public interface MerchantCallbackUrlStrategy
-{
-	String getUrl();
-}
--- core-customize/hybris/bin/custom/zdds/zddsstorefront/web/webroot/WEB-INF/views/addons/profiletagaddon/responsive/cms/profiletagscriptcomponent.jsp
+++
@@ -1,4 +0,0 @@
-<%@ taglib prefix="profiletag" tagdir="/WEB-INF/tags/addons/profiletagaddon/shared/profiletag" %>
-
-<profiletag:profileTag/>
-
